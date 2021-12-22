5d5f1df24782f7ec44abf5f9c5263a659bb544d4
Tim Gale <tim.gale16@gmail.com>
2021-12-22 21:19:18 +0000

    add contact page content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,25 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const ContactPage = () => {
+const ContactPage = ({ data }) => {
   return (
-    <Layout pageTitle="Home">
+    <Layout pageTitle="Contact">
       <h1>Contact</h1>
+
+      <div dangerouslySetInnerHTML={{ __html: data.wpgraphql.page.content }} />
     </Layout>
   )
 }
+
+export const query = graphql`
+    query {
+        wpgraphql {
+            page(id: "cG9zdDoxNA==") {
+                content
+            }
+        }
+    }
+`
 
 export default ContactPage
